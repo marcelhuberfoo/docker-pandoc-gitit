c039df617f76b0408bb5decc9f6cc3770f20db2d
Marcel Huber <marcelhuberfoo@gmail.com>
2016-09-07 17:05:17 +0200

    timeout needs to be command specific, try again

--- build.sh
+++ build.sh
@@ -5,7 +5,12 @@ docker_image=marcelhuberfoo/pandoc-gitit
 docker_file=Dockerfile
 docker_context=.
 ver_file=pandoc_gitit_version.sh
-docker build --rm --tag=${docker_image}:latest --file=$docker_file $docker_context
+docker build --rm \
+             --build-arg BUILD_DATE=$(date -u +"%Y-%m-%dT%H:%M:%SZ") \
+             --build-arg VCS_REF=$(git rev-parse --short HEAD) \
+             --build-arg PANDOC_VERSION=$(./$ver_file f | cut -d"_" -f1) \
+             --build-arg GITIT_VERSION=$(./$ver_file f | cut -d"_" -f2) \
+             --tag=$docker_image --file=$docker_file $docker_context
 
 docker tag ${docker_image}:latest ${docker_image}:$(./$ver_file f)
 
